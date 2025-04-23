e24c9bf8c6e9072dcc7536134cb3d2975b5ebc35
Carla Mariana <carlamab2808@outlook.com>
2025-04-23 00:30:21 -0300

    update

--- components/Primeiro.tsx
+++ components/Primeiro.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { Text } from 'react-native';
-import Estilo from './estilo'
+import { Text } from "react-native";
+import Estilo from "./estilo";
 
 export function Primeiro() {
-  return (
-   <Text style={Estilo.fontG}>Primeiro</Text>
-  );
+  //console.warn('Opa!');
+  return <Text style={Estilo.fontG}>Primeiro</Text>;
 }
